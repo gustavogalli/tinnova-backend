503669771f9b003ecc2b85abeefc7dd2a33eb2a8
Gustavo Galli <ggalli@progressrail.com>
2026-01-29 13:15:06 -0300

    Adding application class

---
+++ src/main/java/com/galli/tinnova/TinnovaApplication.java
@@ -0,0 +1,13 @@
+package com.galli.tinnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinnovaApplication.class, args);
+	}
+
+}
